e37af8e1816b74e0b1ba6e4325dc9b180ebd0f0b
eric kreinar dev <eric.kreinar.dev@irscsv.local>
2019-03-04 12:09:03 -0500

    first commit for push back to github

---
+++ server.js
@@ -0,0 +1,66 @@
+//test request
+
+const express = require('express');
+const hbs = require('hbs');
+// const ejs = require('')
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+var Client = require('node-rest-client').Client;
+ 
+var client = new Client();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+const request = require('request');
+
+app.get('/users', function(req, res) {
+request({
+    url: 'http://locahost:8000/api/users',
+    json: true
+}, (error, response, body) => {
+//console.log(body);
+const users = body.users
+res.render('pages/main.hbs', {
+        users: users
+    });
+
+  });
+});
+app.post('/users', (req, res) => {
+    var text = req.body.tasktext;
+    var assignedTo = req.body.assignedTo;
+    console.log(text);
+    // set content-type header and data as json in args parameter 
+    var args = {
+    data: { text: text,
+          assignedTo: assignedTo},
+    headers: { "Content-Type": "application/json" }
+    };
+    
+    //async await begins
+    client.post('http://52.61.217.55:8001/api', args, function (data, response) {
+    // parsed response body as js object 
+         users = data.users;
+  
+    return users
+    });
+    // redirect client to individual just added chore page
+    res.redirect(307,'/chore-added');
+   
+   
+   });
+app.post('/chore-added', (req, res) => {
+    
+  res.render('pages/chore-success.hbs', {
+    pageTitle: 'Added Chore Page',
+      
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
